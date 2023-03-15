401abcbb22e2a6f51a239ac437f7c3fb906f4851
Paul Stewart <stewart.paul@gmail.com>
2023-03-15 09:56:40 -0400

    Update main.py
    updated

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from urllib.request import urlopen
 import json
 
-url = "https://www.autotrader.com/rest/searchresults/base?startYear=2023&zip=29492&makeCode1=BMW&modelCode1=BMWX7&searchRadius=0&minPrice=80000&maxPrice=89000"
+url = "https://www.autotrader.com/rest/searchresults/base?startYear=2023&zip=29492&makeCode1=BMW&modelCode1=BMWX7&searchRadius=0&minPrice=80000&maxPrice=89000&sortBy=datelistedDESC"
 response = urlopen(url)
 data = json.loads(response.read())
 
 if (data['totalResultCount'] > 0):
+    # for i in data:
+    #    print(json.dumps(i, indent=1))
+    print( data['totalResultCount'])
+    x = 0
     for i in data['listings']:
+        x += 1
+        #   debug
+        # print(i['vin'])
+        if i['vin'] == '5UX23EM04P9N98461':
+            print(json.dumps(i, indent=1))
+
         myprintln = i["title"], "Interior color:", i["specifications"]["interiorColor"]["value"], i["vin"]
         if "packages" in i:
             myprintln = myprintln, i['packages']
@@ -19,15 +29,15 @@ if (data['totalResultCount'] > 0):
             myprintln = myprintln, currency_string
         if "color" in i["specifications"]:
             myprintln = myprintln, "exterior color: ", i["specifications"]["color"]["value"]
-        #   if i['vin'] == '5UX23EM02P9P75217':
-        #       print(json.dumps(i, indent=1))
+
         myprintln = myprintln, i['website']['href']
 
         # Now we're gonna filter
         if (i["specifications"]["interiorColor"]["value"] not in ('Coffee','Cognac','Black','Coffee Sensafin','Coffee W/Sensafin Upholstery','Cognac Sensafin')):
-            if 'Executive Package' in i['packages']:
+            if "packages" in i and 'Executive Package' in i['packages']:
                 # sold
-                if i["vin"] not in ('5UX23EM06P9R78708'):
+                if i["vin"] not  in ('5UX23EM06P9R78708'):
                    print(json.dumps(myprintln, indent=1))
-                   print(i["vin"])
+                   #print(i["vin"])
                    #print(json.dumps(i, indent=1))
+    # print('amount fetched', x)
